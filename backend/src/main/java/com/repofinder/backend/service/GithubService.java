98e580bc6b54d51f7793384506d5fb03d01ab313
Gabriel Figueiredo <eugfl@hotmail.com>
2024-12-05 02:11:34 -0300

    :sparkles: feat(service/GithubService): Introduce `GithubService` to handle user and repository data retrieval from GitHub.

---
+++ backend/src/main/java/com/repofinder/backend/service/GithubService.java
@@ -0,0 +1,29 @@
+package com.repofinder.backend.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.repofinder.backend.client.GithubClient;
+import com.repofinder.backend.controller.RepositoryResponse;
+import com.repofinder.backend.controller.UserData;
+import com.repofinder.backend.controller.UserResponse;
+
+@Service
+public class GithubService {
+
+    private final GithubClient githubClient;
+
+    public GithubService(GithubClient githubClient) {
+        this.githubClient = githubClient;
+    }
+
+    public UserResponse getUserWithRepositories(String username) {
+
+        UserData userData = githubClient.getUser(username);
+
+        List<RepositoryResponse> repositories = githubClient.listRepos(username);
+
+        return new UserResponse(userData.login(), userData.htmlUrl(), repositories, repositories.size());
+    }
+}
