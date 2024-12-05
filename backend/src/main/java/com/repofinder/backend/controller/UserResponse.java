52dff9cd5558e0e43fe7d11bcb2fdfcd77221713
Gabriel Figueiredo <eugfl@hotmail.com>
2024-12-05 02:11:16 -0300

    :sparkles: feat(backend/controller): Add login and htmlUrl fields to `UserResponse` record

--- backend/src/main/java/com/repofinder/backend/controller/UserResponse.java
+++ backend/src/main/java/com/repofinder/backend/controller/UserResponse.java
@@ -3,7 +3,8 @@ package com.repofinder.backend.controller;
 import java.util.List;
 
 public record UserResponse(
-        UserData user,
+        String login,
+        String htmlUrl,
         List<RepositoryResponse> repositories,
         int totalRepositories) {
 }
