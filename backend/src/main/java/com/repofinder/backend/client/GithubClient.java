2d9f5c2a518ebcc6b079033608e0c8d238664e06
Gabriel Figueiredo <eugfl@hotmail.com>
2024-12-05 03:46:52 -0300

    :sparkles: feat(client/GithubClient): Add Spring annotations to `GithubClient` for improved functionality and clarity.

--- backend/src/main/java/com/repofinder/backend/client/GithubClient.java
+++ backend/src/main/java/com/repofinder/backend/client/GithubClient.java
@@ -2,16 +2,19 @@ package com.repofinder.backend.client;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.repofinder.backend.controller.RepositoryResponse;
 import com.repofinder.backend.controller.UserData;
 
+@Component
 public interface GithubClient {
 
     @GetExchange("/users/{username}")
-    public UserData getUser(String username);
+    UserData getUser(@PathVariable String username);
 
     @GetExchange("/users/{username}/repos")
-    public List<RepositoryResponse> listRepos(String username);
+    List<RepositoryResponse> listRepos(@PathVariable String username);
 }
