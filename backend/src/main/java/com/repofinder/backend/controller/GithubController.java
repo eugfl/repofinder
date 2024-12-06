084722199e66d5f77abb15ea34d47c6253dca0c7
Gabriel Figueiredo <eugfl@hotmail.com>
2024-12-06 00:44:16 -0300

    :sparkles: feat(config): Add CORS configuration to allow requests from localhost:3000

--- backend/src/main/java/com/repofinder/backend/controller/GithubController.java
+++ backend/src/main/java/com/repofinder/backend/controller/GithubController.java
@@ -1,6 +1,7 @@
 package com.repofinder.backend.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.repofinder.backend.model.UserResponse;
 import com.repofinder.backend.service.GithubService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/github")
 public class GithubController {
