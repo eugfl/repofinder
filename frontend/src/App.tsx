59e912c5b368df6be7ef3465e7b34ee59276d8bd
Gabriel Figueiredo <eugfl@hotmail.com>
2024-12-06 00:45:06 -0300

    :lipstick: style(App): Enhance layout and styling of the App component, adding a form and toast notifications.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Form from "./components/Form";
+
 function App() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gray-800 text-white">
-      <h1 className="text-4xl font-lobster">RepoFinder</h1>
-      <h2 className="text-xl mt-2 text-gray-400 font-sans">
-        Seu buscador de repositórios
-      </h2>
-    </div>
+    <section className="min-h-screen flex flex-col justify-center items-center bg-gray-800 text-white px-4">
+      <div className="text-center mb-8">
+        <h1 className="text-8xl font-lobster">RepoFinder</h1>
+        <h2 className="text-xl mt-2 text-gray-400 font-sans">
+          Seu buscador de repositórios
+        </h2>
+      </div>
+
+      <Form />
+
+      <ToastContainer />
+    </section>
   );
 }
 
