2a94ec226965145cf14b0374473ddcfae6b09c98
Gabriel Figueiredo <eugfl@hotmail.com>
2024-12-06 23:46:30 -0300

    :sparkles: feat(backend): Add Docker support and enhance security configuration.

--- backend/src/main/java/com/repofinder/backend/controller/GithubController.java
+++ backend/src/main/java/com/repofinder/backend/controller/GithubController.java
@@ -1,7 +1,6 @@
 package com.repofinder.backend.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.repofinder.backend.model.UserResponse;
 import com.repofinder.backend.service.GithubService;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/github")
 public class GithubController {
